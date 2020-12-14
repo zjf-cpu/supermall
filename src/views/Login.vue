a95fdf48216ac3d2151e519587c35da6332f9424
Your Name <1137183005@qq.com>
2020-12-14 17:18:59 +0800

    '初始化项目'

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login_container">
+    <video width="100%" height="100%" id="myVideo" class="video-js video" style="object-fit: fill;">
+      <source src="../../../founder_vue-dev_shce-8d71e5509fb5ef914da98852d1cd49bbed845b8b/src/assets/img/Login/animate.mp4" type="video/mp4">
+      您的浏览器不支持 video 标签。
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
